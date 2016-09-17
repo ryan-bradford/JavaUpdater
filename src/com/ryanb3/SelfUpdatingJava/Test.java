7bf65c6c8d6135f57c4df8c9a1b6142df2173d6a
ryanb <ryanbradford333@gmail.com>
2016-09-17 16:42:26 -0400

    Added the very minimal function to the library. Will add more features later. Just testing now.

---
+++ src/com/ryanb3/SelfUpdatingJava/Test.java
@@ -0,0 +1,17 @@
+package com.ryanb3.SelfUpdatingJava;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		/*
+		try {
+			new Main("http://rbradford.thaumavor.io/Test/", "Test", "index.txt", "1.0");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+	}
+	
+}
---
+++ src/com/ryanb3/SelfUpdatingJava/Update.java
@@ -0,0 +1,76 @@
+package com.ryanb3.SelfUpdatingJava;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Update {
+
+	public Update(String URL, String programName, String indexName, String currentIndex)
+			throws MalformedURLException, IOException {
+		String newIndex = checkIndex(URL + indexName);
+		if (!newIndex.equals(currentIndex)) {
+			try {
+				String newName = programName + " " + newIndex + ".jar";
+				downloadJar(programName, newName, URL);
+				launchJar(newName);
+				deleteOldJar(programName, currentIndex);
+			} catch(IOException ex) {
+				
+			}
+		}
+	}
+	
+	public void deleteOldJar(String programName, String oldIndex) {
+		File file = new File(programName + " " + oldIndex + ".jar");
+		file.delete();
+	}
+	
+	public void launchJar(String jarName) throws IOException {
+		Process ps=Runtime.getRuntime().exec(new String[]{"java", "-jar", jarName});			
+	}
+	
+	public void downloadJar(String programName, String jarName, String URL) throws IOException {
+		try (ReadableByteChannel in = Channels.newChannel(new URL(URL + programName + ".jar").openStream());
+			FileChannel out = new FileOutputStream(jarName).getChannel()) {
+			out.transferFrom(in, 0, Long.MAX_VALUE);
+		}
+	}
+
+	public String dealWithSpaces(String toProcess) {
+		String[] array = toProcess.split(" ");
+		String toReturn = "";
+		for (int i = 0; i < array.length; i++) {
+			toReturn += array[i];
+			if (i != array.length - 1) {
+				toReturn += new String("\\ ");
+			}
+		}
+		return toReturn;
+	}
+
+	public String checkIndex(String URL) {
+		String content = null;
+		URLConnection connection = null;
+		try {
+			connection = new URL(URL).openConnection();
+			Scanner scanner = new Scanner(connection.getInputStream());
+			scanner.useDelimiter("\\Z");
+			content = scanner.next();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return content;
+
+	}
+
+}
